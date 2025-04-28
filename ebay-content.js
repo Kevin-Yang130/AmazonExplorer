69cda8f865fd7edd4377e836630e3f319dde9264
Wesley Liu <152113348+wesleyyliu@users.noreply.github.com>
2025-04-28 04:37:53 -0400

    ebay-content.js changes

--- ebay-content.js
+++ ebay-content.js
@@ -1,33 +1,3 @@
-function extractOneReview() {
-    console.log('Starting to extract one review...');
-    
-    // Try to find the first review text
-    const reviewHeader = document.querySelector('[data-hook="review-title"]');
-    const spans = reviewHeader?.querySelectorAll('span') || [];
-    const reviewTitle = spans.length ? spans[spans.length - 1].textContent.trim(): "";
-
-    const titleSpan = document.querySelector('[data-hook="review-title"] span:not(.a-letter-space)');
-    const reviewStars = titleSpan?.textContent.trim()[0] || "";
-
-    const reviewText = document.querySelector('[data-hook="review-collapsed"]')?.textContent.trim();
-
-    if (!reviewText) {
-        console.log('No review text found');
-        return { success: false };
-    }
-    console.log("Review Title: " + reviewTitle)
-    console.log("Review stars: " + reviewStars)
-    console.log("Review Text: " + reviewText)
-
-
-    return {
-        success:true,
-        reviewTitle,
-        reviewStars,
-        reviewText,
-      };
-}
-
 function extractAllReviews() {
     console.log('Extracting all reviews...');
   
